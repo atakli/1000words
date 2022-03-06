59b326d93b97d1834ff3110ce6b42510c567d5ba
Emre ATAKLI <atakliemre93@metu.edu.tr>
2022-03-06 14:12:55 +0300

    hadi gayret, 2 tane kaldı

--- pdff.py
+++ pdff.py
@@ -5,6 +5,9 @@ git config --global credential.helper 'cache --timeout=600'
 # çok hoş: (file contentini clipboard'a kopyala. başka güzel şeyleri de varmış xclip'in. man'ı oku diyorlar)
 xclip -sel c < githubToken.txt
 
+#import collections # used_words'daki duplicate'leri bulmak için
+#print([item for item, count in collections.Counter(used_words).items() if count > 1])
+
 risale = "/home/b720/Desktop/1000words/collection/Signs+of+Miraculousness+-+Vahide.pdf"
 from PyPDF4.pdf import PdfFileReader
 reader = PdfFileReader(open(risale, "rb"))
@@ -70,13 +73,19 @@ for word in unused_words:
 	unused_words_new.append(word + '\n')
 with open('/home/b720/Desktop/1000words/1000 Words/kullanılmamış_kelimeler.txt','w') as file:
 	file.writelines(unused_words_new)
-		
+"""		
 noluyor = []
 for i in used_words:
 	if i not in all_words_new:
 		noluyor.append(i)
 print(*noluyor,sep='\n')
-		
+"""
+noluyor = []
+for i in unused_words:
+	if i not in all_words:
+		noluyor.append(i)
+print(*noluyor,sep='\n')
+	
 for word in all_words:
 	if word not in used_words:
 		findings = list(find_all(text, word))
